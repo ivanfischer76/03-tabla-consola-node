3cb66e8cabc9cfa957928bac2d46695a16209e0c
Iván Fischer <ivanfischer76@gmail.com>
2021-03-26 20:12:21 -0300

    Modificación de comentario en app.js-26-03-2021

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//para utilizar correr en consola:
+// node app -b=[base] -i=[inicio] -f=[fin] -l
+
 // importamos el archivo que realiza la operación
 const { boolean } = require('yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
@@ -9,15 +12,3 @@ crearArchivo(argv.b, argv.i,  argv.f, argv.l)
     .then(nombreArchivo => console.log(colors.brightBlue.italic.bold(nombreArchivo, 'creado')))
     .catch(err => console.log(err));
 
-
-
-
-
-
-//console.clear();
-//console.log(argv);
-//console.log('base: yargs', argv.base);
-// esto no se usa
-// console.log(process.argv);
-// const [ , , arg3 = 'base=5'] = process.argv;
-// const [ , base = 5] = arg3.split('=');
